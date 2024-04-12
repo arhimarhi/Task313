652dc5dcb4b515f11aa596adf0dbc39a15eff9c3
arhimarhi <dmitrieva.kg@gmail.com>
2024-04-12 10:36:11 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        boolean b = false;
+        int s = 0;
+        int[] a = new int[n];
+        for (int i = 0; i < n; ++i) {
+            a[i] = scanner.nextInt();
+        }
+        int jj = 0;
+        for (int i = 0; i < n; ++i) {
+            b = false;
+            for (int j = i + 1; j < n; ++j) {
+                if (a[i] == a[j]) {
+                    jj = j;
+                    b = true;
+                    break;
+                }
+            }
+            if (b) {
+                if (jj - i > s) {
+                    s = jj - i;
+                }
+            }
+        }
+        System.out.println(s);
     }
 }
